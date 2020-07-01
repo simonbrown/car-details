eb0bd214b4dcfa19a1ec6cedb35e95d1e226454a
Simon Brown <simon@simonandrewbrown.co.uk>
2020-07-01 19:32:52 +0100

    * CarController.cs:
    * MOTController.cs: Add basic controller to proxy government MOT
      service

---
+++ car-details/Controllers/CarController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace car_details.Controllers
+{
+    [ApiController]
+    [Route("car/{registration}")]
+    public class CarController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string registration)
+        {
+            String requestUrlTemplate = "https://beta.check-mot.service.gov.uk/trade/vehicles/mot-tests?registration={0}";
+            String requestUrl = String.Format(requestUrlTemplate, registration);
+
+            string mot_api_key = Environment.GetEnvironmentVariable("MOT_API_KEY");
+            if (mot_api_key == null)
+            {
+                throw new ApplicationException("MOT_API_KEY not specified");
+            }
+
+            WebRequest request = WebRequest.Create(requestUrl);
+            request.Headers.Add("x-api-key", mot_api_key);
+
+            using (WebResponse webResponse = request.GetResponse())
+            {
+                StreamReader reader = new StreamReader(webResponse.GetResponseStream());
+                string responseStr = reader.ReadToEnd();
+                reader.Close();
+
+                return responseStr;
+            }
+        }
+    }
+}
